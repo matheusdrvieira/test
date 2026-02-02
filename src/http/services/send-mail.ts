429976d6dbb73d2e0fe08d4d56ee01c5c18c3d1a
Matheus Vieira <rochavieira.matheus@gmail.com>
2026-02-02 17:30:36 -0300

    first commit

--- src/http/services/send-mail.ts
+++ src/http/services/send-mail.ts
@@ -10,6 +10,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+    requireTLS: true,
     connectionTimeout: 5000,
     greetingTimeout: 5000,
     pool: true,
