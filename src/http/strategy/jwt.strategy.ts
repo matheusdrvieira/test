21fb564843a1f38c9d988c973c274f0a0d709a21
Matheus Vieira <rochavieira.matheus@gmail.com>
2026-02-02 15:32:49 -0300

    first commit

--- src/http/strategy/jwt.strategy.ts
+++ src/http/strategy/jwt.strategy.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { createRemoteJWKSet, jwtVerify } from "jose";
 
 export interface AuthPayload {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import { openapiPlugin } from '@/http/plugins/openapi';
 import { auth } from '@/lib/auth';
 import { cors } from "@elysiajs/cors";
